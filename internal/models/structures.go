f40b49cbe04db5f2c5b0c56b285e7a5e73af00c4
gulzada <242194@astanait.edu.kz>
2026-01-23 17:40:06 +0500

    Gulzada's commit

--- internal/models/structures.go
+++ internal/models/structures.go
@@ -1,1 +1,19 @@
 package models
+
+type User struct {
+	ID       int
+	Email    string
+	Password string
+}
+
+type Book struct {
+	ID    int
+	Title string
+	Price float64
+}
+
+type Order struct {
+	ID     int
+	UserID int
+	Total  float64
+}
