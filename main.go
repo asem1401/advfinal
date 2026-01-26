84246e1878b0a6679ac91e7b7de377ccb8a18969
gulzada <242194@astanait.edu.kz>
2026-01-26 19:09:53 +0500

    Update main.go

--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"bookstore/internal/handlers"
+	"bookstore/internal/logic"
 )
 
 func main() {
+	logic.StartCartWorkerPool(3)
+
 	mux := http.NewServeMux()
+
 	RegisterRoutes(mux)
+
+	mux.HandleFunc("/cart/add", handlers.AddToCartHandler)
+
 	log.Println("Server started at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
