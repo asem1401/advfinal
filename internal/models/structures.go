ec804cccd90e3f8309122bb74aebf1d73c86ee04
asem1401 <kadulbekovaasem@gmail.com>
2026-01-26 17:21:13 +0500

    Backend logic and repository implementation

--- internal/models/structures.go
+++ internal/models/structures.go
@@ -1,1 +1,48 @@
-package models
+type Book struct {
+	ID          int
+	Title       string
+	Author      string
+	Genre       string
+	Price       float64
+	Description string
+}
+
+type Customer struct {
+	ID       int
+	Email    string
+	Password string
+	Address  string
+}
+
+type Cart struct {
+	ID         int
+	CustomerID int
+	CreatedAt  time.Time
+}
+
+type CartItem struct {
+	ID     int
+	CartID int
+	BookID int
+	Qty    int
+}
+
+type Order struct {
+	ID         int
+	CustomerID int
+	Total      float64
+}
+
+type OrderItem struct {
+	ID      int
+	OrderID int
+	BookID  int
+	Price   float64
+}
+
+type Payment struct {
+	ID      int
+	OrderID int
+	Total   float64
+	Status  string
+}
