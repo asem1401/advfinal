594134bf259c58619a3a9b940a3b00cd777946f9
gulzada <242194@astanait.edu.kz>
2026-01-28 15:42:27 +0500

    Remove AddToCartHandler route and import
    Removed the AddToCartHandler route and the handlers import.

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"net/http"
 
-	"bookstore/internal/handlers"
 	"bookstore/internal/logic"
 )
 
@@ -12,10 +11,7 @@ func main() {
 	logic.StartCartWorkerPool(3)
 
 	mux := http.NewServeMux()
-
 	RegisterRoutes(mux)
-
-	mux.HandleFunc("/cart/add", handlers.AddToCartHandler)
 
 	log.Println("Server started at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
