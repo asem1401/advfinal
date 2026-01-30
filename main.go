ead04e289df27d7849947a2b6b1cf2ba8a4d7ec2
gulzada <242194@astanait.edu.kz>
2026-01-30 18:14:37 +0500

    Initialize cart worker pool in main function
    Added StartCartWorkerPool to initialize the worker pool.

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
-	"log"
-	"net/http"
+ "log"
+ "net/http"
+
+ "bookstore/internal/logic"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	RegisterRoutes(mux)
-	log.Println("Server started at http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+ logic.StartCartWorkerPool(3)
+
+ mux := http.NewServeMux()
+ RegisterRoutes(mux)
+
+ log.Println("Server started at http://localhost:8080")
+ log.Fatal(http.ListenAndServe(":8080", mux))
 }
